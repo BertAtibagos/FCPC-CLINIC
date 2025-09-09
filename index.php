215c21948114885b3848d1163c27cdc8cf20498a
BertAtibagos <atibagos.robert2300@gmail.com>
2025-09-09 16:48:54 +0800

    Start of UI

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <html>
     <head>
-
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
     </head>
     <body>
-        Hello
+        <section>
+            <div class="searchStudent">
+                <input class="searchStudent srch" type="search" id="studSearch" name="studSearch">
+                <select class="searchStudent Catg">
+                    <option selected disabled>All</option>
+                    <option value="2">Student</option>
+                    <option value="1">Employee</option>
+                </select>
+                <button id="studSearchBtn">Search</button> 
+            </div>
+        </section>
     </body>
 </html>
