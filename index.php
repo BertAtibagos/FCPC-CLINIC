655e4d512ddd767f905174f4413c364cdc9a0859
BertAtibagos <atibagos.robert2300@gmail.com>
2025-09-23 16:56:02 +0800

    Make the information section dynamic based on history or new option.

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         </div>
 
         <!-- Start of Search Result table-->
-        <section class="search-result-section hidden" id="searchResultSection">
+        <section class="search-result-section" id="searchResultSection">
             <div class="search-result card">
                 <h3>TRIAGE RECORDS</h3>
                 <table class="table table-bordered">
@@ -64,17 +64,6 @@
         <section class="person-record-section-form">
             <div class="person-record card">
                 <div class="person-record-header">
-                    <div class="person-img">
-                        <img src="attachment/user.jpg" alt="user image" id="person_profile">
-                    </div>
-                    <div class="person-info">
-                        <h3 class="person-info-fullname">Robert Galapin Atibagos</h3>
-                        <p class="person-info-grdSec">Doctorate of Undergraduate of Masters of Bachelors of Science in High Information Technology (DUMBSHIT 1A)</p>
-                        <div class="person-info-ageGender">
-                            <p>Age: 25</p>
-                            <p>Gender: Male</p>
-                        </div>
-                    </div>
                 </div>
 
                 <div class="person-triage-info">
@@ -97,94 +86,7 @@
                     </div>
 
                     <div class="person-triage-info-record">
-                        <form id="triage-info-form">
-                            <div class="triage-info-form-wrap">
-
-                                <div class="triage-info-form-pt1">
-                                    <b>Time and Date of Visit</b>
-                                    <div class="d-flex flex-row">
-                                        <label for="triageInfoForm_date" class="form-label">Date</label>
-                                        <input type="date" class="form-control form-date" id="triageInfoForm_date" value="<?php echo date('Y-m-d'); ?>" required>
-
-                                        <label for="triageInfoForm_time" class="form-label">Time<span
-                                            class="text-danger">*</span></label>
-                                        <input type="time" class="form-control form-time" id="triageInfoForm_time" required>
-                                    </div>
-                                    <div>
-                                        <label for="triageInfoForm_reason" class="form-label form-label-reason">Reason for Clinic Visit<span
-                                            class="text-danger">*</span></label>
-                                        <textarea class="form-control form-reason" id="triageInfoForm_reason" aria-describedby="visitReason" required></textarea>
-                                    </div>
-                                </div>
-                                
-                                <div class="triage-info-form-pt2">
-                                    <b>Vitals</b>
-                                    <div class="triage-info-vitals">
-                                        <div class="triage-info-vitals1">
-                                            <div>
-                                                <label for="vit_bp">BP:</label>
-                                                <input type=text id="vit_bp" class="line-input vital-input">
-                                            </div>
-                                            <div>
-                                                <label for="vit_hr">HR:</label>
-                                                <input type=text id="vit_hr" class="line-input vital-input">
-                                            </div>
-                                        </div>
-                                        <div class="triage-info-vitals2">
-                                            <div>
-                                                <label for="vit_rr">RR:</label>
-                                                <input type=text id="vit_rr" class="line-input vital-input">
-                                            </div>
-                                            <div>
-                                                <label for="vit_02">O₂ SAT:</label>
-                                                <input type=text id="vit_02" class="line-input vital-input">
-                                            </div>
-                                        </div>
-                                        <div class="triage-info-vitals3">
-                                            <div>
-                                                <label for="vit_temp">TEMP:</label>
-                                                <input type=text id="vit_temp" class="line-input vital-input">
-                                            </div>
-                                            <div>
-                                                <label for="vit_ht">Height:</label>
-                                                <input type=text id="vit_ht" class="line-input vital-input">
-                                            </div>
-                                        </div>
-                                        <div class="triage-info-vitals4">
-                                            <div>
-                                                <label for="vit_wt">Weight:</label>
-                                                <input type=text id="vit_wt" class="line-input vital-input">
-                                            </div>
-                                            <div>
-                                                <label for="vit_bmi">BMI:</label>
-                                                <input type=text id="vit_bmi" class="line-input vital-input">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="triage-info-form-pt3">
-                                    <b>History</b>
-                                    <div>
-                                        <label for="triageInfoForm_prior" class="form-label">Prior s/sx</label>
-                                        <textarea class="form-control form-prior" id="triageInfoForm_prior" aria-describedby="visitReason"></textarea>
-                                    </div>
-                                    <div>
-                                        <label for="triageInfoForm_prior" class="form-label">Present s/sx</label>
-                                        <textarea class="form-control form-present" id="triageInfoForm_prior" aria-describedby="visitReason"></textarea>
-                                    </div>
-                                    <div>
-                                        <label for="triageInfoForm_prior" class="form-label">Intervention</label>
-                                        <textarea class="form-control form-interv" id="triageInfoForm_prior" aria-describedby="visitReason"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="triage-info-form-confirm">
-                                <input type="checkbox" id="confirm_new_record" value="1" name="confirm_new_record" required>
-                                <label for="confirm_new_record">I confirm the information above is correct</label>
-                            </div>
-                            <button type="submit" class="btn submit-btn" id="submit_new_rcrd_btn" disabled>Submit</button>
-                        </form>
+                        
                     </div>
                 </div>
             </div>
@@ -192,7 +94,7 @@
         <!-- End of person new records -->
 
         <!-- Start of person history records -->
-        <section class="person-record-section">
+        <!-- <section class="person-record-section">
             <div class="person-record card">
                 <div class="person-record-header">
                     <div class="person-img">
@@ -302,7 +204,7 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
         <!-- End of person history records -->
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
